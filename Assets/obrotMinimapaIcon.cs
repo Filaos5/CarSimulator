a62de69bea969f7f6895a4560ef63609b87c7d90
JohnWolf12 <82612279+JohnWolf12@users.noreply.github.com>
2024-06-02 10:24:21 +0200

    Statyczne znaczniki wyscigow na mapie

--- Assets/obrotMinimapaIcon.cs
+++ Assets/obrotMinimapaIcon.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class obrotMinimapaIcon : MonoBehaviour
 {
     public Camera minimap_kamera;
+    public GameObject mapa;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,17 @@ public class obrotMinimapaIcon : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.rotation = Quaternion.Euler(90.0f, minimap_kamera.transform.eulerAngles.y, 0.0f);
+        GameManager gameManager = mapa.GetComponent<GameManager>();
+        int wlmapa = gameManager.mapa_p;
+        if (wlmapa == 0)
+        {
+            transform.rotation = Quaternion.Euler(90.0f, minimap_kamera.transform.eulerAngles.y, 0.0f);
+            transform.localScale = new Vector3(8, 8, 4);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(180.0f, 0.0f, 0.0f);
+            transform.localScale = new Vector3(24, 24, 12);
+        }
     }
 }
