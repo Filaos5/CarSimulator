99c014f8df51a21e6380a2f2865e857358794cc1
JohnWolf12 <82612279+JohnWolf12@users.noreply.github.com>
2024-06-02 12:52:41 +0200

    Skalowanie ikon mapa

--- Assets/StrzalkaGracza.cs
+++ Assets/StrzalkaGracza.cs
@@ -8,6 +8,7 @@ public class StrzalkaGracza : MonoBehaviour
     public GameObject kamera;
     public string nazwa_pojazdu;
     public int rodzaj_pojazdu;
+    public Camera mapa_kamera;
     void Start()
     {
         transform.position = new Vector3(kamera.transform.position.x, kamera.transform.position.y + 110, kamera.transform.position.z);
@@ -23,6 +24,7 @@ public class StrzalkaGracza : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(-90.0f, 0.0f, -180.0f);
         }
+        transform.localScale = new Vector3(50 + mapa_kamera.orthographicSize * 0.03f, 50 + mapa_kamera.orthographicSize * 0.03f, 1 + mapa_kamera.orthographicSize * 0.03f);
     }
 
     // Update is called once per frame
@@ -41,5 +43,6 @@ public class StrzalkaGracza : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(-90.0f, 0.0f, -180.0f);
         }
+        transform.localScale = new Vector3(50 + mapa_kamera.orthographicSize * 0.03f, 50 + mapa_kamera.orthographicSize * 0.03f, 1 + mapa_kamera.orthographicSize * 0.03f);
     }
 }
--- Assets/ikonaSamochod.cs
+++ Assets/ikonaSamochod.cs
@@ -8,6 +8,8 @@ public class ikonaSamochod : MonoBehaviour
     public Camera minimap_kamera;
     public string nazwa_pojazdu;
     public int rodzaj_pojazdu;
+    public GameObject mapa;
+    public Camera mapa_kamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +19,34 @@ public class ikonaSamochod : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.rotation = Quaternion.Euler(90.0f, minimap_kamera.transform.eulerAngles.y, 0.0f);
+        GameManager gameManager = mapa.GetComponent<GameManager>();
+        int wlmapa = gameManager.mapa_p;
         Kamera kamera_o = kamera.GetComponent<Kamera>();
         nazwa_pojazdu = kamera_o.nazwa_aktualnego_pojazdu;
         rodzaj_pojazdu = kamera_o.obiekt_rodzaj;
-        if (rodzaj_pojazdu == 1 && nazwa_pojazdu == transform.parent.name)
+        if (wlmapa == 0)
         {
-            transform.localScale = Vector3.zero;
+            transform.rotation = Quaternion.Euler(90.0f, minimap_kamera.transform.eulerAngles.y, 0.0f);
+            if (rodzaj_pojazdu == 1 && nazwa_pojazdu == transform.parent.name)
+            {
+                transform.localScale = Vector3.zero;
+            }
+            else
+            {
+                transform.localScale = new Vector3(4f, 4f, 1f);
+            }
         }
         else
         {
-            transform.localScale = new Vector3(4f, 4f, 1f);
+            transform.localRotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            if (rodzaj_pojazdu == 1 && nazwa_pojazdu == transform.parent.name)
+            {
+                transform.localScale = Vector3.zero;
+            }
+            else
+            {
+                transform.localScale = new Vector3(4 + mapa_kamera.orthographicSize * 0.01f, 4 + mapa_kamera.orthographicSize * 0.01f, 1 + mapa_kamera.orthographicSize * 0.01f);
+            }
         }
     }
 }
